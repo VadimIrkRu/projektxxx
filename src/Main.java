209fc62b5735ab7018ab2cffb8fd68968408a997
Мария <irktel@gmail.com>
2025-04-19 19:23:52 +0300

    к уроку в youtube

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        //comment
+        System.out.print("Все что напишу \\\n\t\"напечатается в консоли\" ");
+/*comment
+ comment1
+ comment2
+*/
+    }
+}
