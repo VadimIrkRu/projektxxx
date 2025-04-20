d63761a774b77cbde1f4b14953eaf6ecd6bbb1a5
Мария <irktel@gmail.com>
2025-04-20 18:57:45 +0300

    Ввод данных от пользователя

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,22 @@
+
+import java.util.Scanner;
+// импортировали Scanner
 public class Main {
     public static void main(String[] args){
-        //comment
-        System.out.println("Переменные и типы данных");
+        Scanner scan = new Scanner(System.in);
+        //создали строку для пользователя
+        System.out.print("Введите ваше имя: ");
+        // просто печатаем текст
+        String username = scan.nextLine();
+        // получаем строку от пользователя!!!!
+        System.out.print("Привет: " + username);
+        // печатаем текст  Привет и строку которую получили от пользователя
 
-        byte age = 45;
-        // byte -128 до 127 1 байт
-        short num = 800;
-        // short -32768 до 32767  2 байта
-        int num2 = 25489512;
-        //int -2147483648 до 2147483647 4 байта
-        long num3 = 845812654;
-        // long очень длинное для космоса!!!
-
-        // числа с точкой float в конце обязательно ставить f!!!
-        float num4 = 5.15486f;
-        // duoble много знаков после запятой - тоже для космоса!!!
-        double num5 = 5.156456789;
-
-        // символ char в одинарных кавычках!!! только один символ!!!
-        char numch = 'x';
-
-        // много символов string в двойных кавычках!!!
-        String user_name = "Vadim";
-
-        // boolean только два значения true или false!!
-        boolean isHappy = true;
-
-        System.out.println(user_name);
+        // вводим то что хотим получить от пользователя - переменная называется scan
+        int num1 = scan.nextInt();
+        byte num2 = scan.nextByte();
+        boolean b = scan.nextBoolean();
+        float num3 = scan.nextFloat();
         
 
     }
