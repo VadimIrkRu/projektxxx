d4f6960ead1f045092e2465ef3173180d8fd2919
Мария <irktel@gmail.com>
2025-04-20 10:23:33 +0300

    переменные, целые большие и маленькие , буквенные, символьные.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,31 @@ public class Main {
         //comment
         System.out.println("Переменные и типы данных");
 
-      int age = 56;
-        System.out.println("Возраст: " + age + " лет");
-        age = 24;
-        System.out.println("Возраст: " + age + " года");
+        byte age = 45;
+        // byte -128 до 127 1 байт
+        short num = 800;
+        // short -32768 до 32767  2 байта
+        int num2 = 25489512;
+        //int -2147483648 до 2147483647 4 байта
+        long num3 = 845812654;
+        // long очень длинное для космоса!!!
+
+        // числа с точкой float в конце обязательно ставить f!!!
+        float num4 = 5.15486f;
+        // duoble много знаков после запятой - тоже для космоса!!!
+        double num5 = 5.156456789;
+
+        // символ char в одинарных кавычках!!! только один символ!!!
+        char numch = 'x';
+
+        // много символов string в двойных кавычках!!!
+        String user_name = "Vadim";
+
+        // boolean только два значения true или false!!
+        boolean isHappy = true;
+
+        System.out.println(user_name);
+        
 
     }
 }
